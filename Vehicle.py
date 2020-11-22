44fbd80a6de4da69f0837f30747931e104412abd
kim <kimrop19@gmail.com>
2020-11-22 23:22:27 +0300

    add read me

--- Vehicle.py
+++ Vehicle.py
@@ -16,15 +16,3 @@ class Vehicle(object):
 
     def get_last_street_entry(self):
         return self.travel_time[-1]['entry_time'], self.travel_time[-1]['entry_pos']
-
-
-def print_usage():
-    """Print a guide on how to use the program."""
-    print("USAGE: python [3] traffic.py [-b B] [-c C] [-t T]")
-    print("where:")
-    print("-b: sets the probability of braking. It should be")
-    print("a number between 0 and 1.")
-    print("-c: sets the collision probability. It should be")
-    print("a number between 0 and 1.")
-    print("-t: set the number of iterations to perform")
-    print("the simulation.")
